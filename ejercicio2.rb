791765e097255799077ef529f3094ccdc1f21424
Daniela Fernandez <daniela.fernandez7@gmail.com>
2019-05-15 21:57:23 -0400

    ejercicios completos y funcionando

--- ejercicio2.rb
+++ ejercicio2.rb
@@ -1,46 +1,47 @@
 module Formula
   def perimetro
     if self.class == Rectangulo
-      puts "Rectangulo tiene un perimetro de #{base*2 + altura*2}"
+      puts "Rectangulo tiene un perimetro de #{@largo*2 + @ancho*2}"
     elsif self.class == Cuadrado
-      puts "Cuadrado tiene un perimetro de #{lado*4}"
+      puts "Cuadrado tiene un perimetro de #{@lado*4}"
+    end
   end
 
   def area
     if self.class == Rectangulo
-      puts "Rectangulo tiene un área de #{base*altura}"
+      puts "Rectangulo tiene un área de #{@largo*@ancho}"
     elsif self.class == Cuadrado
-      puts "Cuadrado tiene un área de #{lado**2}"
+      puts "Cuadrado tiene un área de #{@lado**2}"
     end
   end
 end
 
-  class Rectangulo
-    attr_reader :base, :altura
-    include Formula
-    def initialize(largo, ancho)
-      @largo = largo
-      @ancho = ancho
-    end
-
-    def lados
-      print "#{largo} y #{ancho}"
-    end
+class Rectangulo
+  attr_reader :base, :altura
+  include Formula
+  def initialize(largo, ancho)
+    @largo = largo
+    @ancho = ancho
   end
 
-  class Cuadrado
-    attr_reader :lado
-    include Formula
-    def initialize(lado)
-      @lado = lado
-    end
-
-    def lados
-      print "#{lado}"
-    end
+  def lados
+    print "#{largo} y #{ancho}"
   end
 end
 
+class Cuadrado
+  attr_reader :lado
+  include Formula
+  def initialize(lado)
+    @lado = lado
+  end
+
+  def lados
+    print "#{lado}"
+  end
+end
 
 puts Rectangulo.new(20, 30).perimetro
+puts Rectangulo.new(20, 30).area
 puts Cuadrado.new(40).perimetro
+puts Cuadrado.new(40).area
